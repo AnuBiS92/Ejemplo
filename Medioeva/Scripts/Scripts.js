40fe51c55bdc2088e31a92e6f4c5cfa4b05f33e7
Emiliano Alvarez <alvarez.emilianofortin@gmail.com>
2022-04-25 19:09:57 -0300

    Inicialización del repositorio

---
+++ Medioeva/Scripts/Scripts.js
@@ -0,0 +1,32 @@
+let i = 36;
+let s = "Hola";
+let b = false;
+let u;
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+const CONSTANTE = "Texto";
+console.log(CONSTANTE);
+console.log("Valor de s: ",s);
+console.log(Math.sqrt(10));
+console.log(typeof(b));
+console.log(typeof(u));
+let ejemplo = Math.random();
+let rango = 9;
+ejemplo = Math.floor(ejemplo*rango);
+console.log(ejemplo);
+let bsd = prompt("Ingrese el número 8");
+if (bsd==8) {
+    alert("Gracias por ingresar el número 8");
+}
+else {
+    alert("Por favor ingrese el número 8 en el prompt");
+    let bsd = prompt("Ingrese el número 8");
+}
+for (asd = 0;asd <= 10;asd=prompt("Ingrese un nuevo valor para la variable asd")) {
+    alert("La variable asd es: "+asd+". A continuación le pediermos que ingrese un nuevo valor para ASD");
+    
+}
+
+
+
